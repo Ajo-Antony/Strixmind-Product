fb788b1c6a3fb2a875667bac5fde9aab67a72a11
Ajo-Antony <ajoantony19@gmail.com>
2026-07-13 15:36:19 +0530

    Added settings and full steps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  distDir: process.env.NODE_ENV === 'production' ? '.next' : '.next-dev',
 }
 module.exports = nextConfig
